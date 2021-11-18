dff0433c39f76c6dee366c6ce623caad61cb51da
hampuslundblad <31845093+hampuslundblad@users.noreply.github.com>
2021-11-18 23:39:14 +0100

    added begin, end functions to be used with the mkrrgb display in smce-gd

--- include/Ardrivo/SMCE_framebuffer.hpp
+++ include/Ardrivo/SMCE_framebuffer.hpp
@@ -1,12 +1,15 @@
 #include "SMCE_dll.hpp"
 #include <cstdint>
 
+
 class SMCE__DLL_RT_API SMCE_Framebuffer {
   public:
     SMCE_Framebuffer(int id) {
         m_id = id;
     }
     void set (int x, int y, uint8_t r, uint8_t g, uint8_t b);
+    int begin (std::uint16_t width, std::uint16_t height);
+    void end ();
   private:
     int m_id;
 };
