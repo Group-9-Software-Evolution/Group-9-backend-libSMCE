49478986eebca35d5c1225a1c35da3b0d445b15d
nojjeN <niklas.adam.andersson@gmail.com>
2021-11-30 00:39:18 +0100

    Make SMCE_Framebuffer constructor explicit

--- include/Ardrivo/SMCE_framebuffer.hpp
+++ include/Ardrivo/SMCE_framebuffer.hpp
@@ -5,7 +5,7 @@
 
 class SMCE__DLL_RT_API SMCE_Framebuffer {
   public:
-    SMCE_Framebuffer(int id) {
+    explicit SMCE_Framebuffer(int id) {
         m_id = id;
     }
     void set (int x, int y, uint8_t r, uint8_t g, uint8_t b);
